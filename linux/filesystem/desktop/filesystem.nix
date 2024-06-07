3f3f68759292d21cbd3425be19aa2e5bc3a96f7e
dscv101 <88686701+dscv101@users.noreply.github.com>
2024-06-07 17:20:04 +0000

    Update filesystem.nix

--- linux/filesystem/desktop/filesystem.nix
+++ linux/filesystem/desktop/filesystem.nix
@@ -8,7 +8,7 @@
   fileSystems."/" = {
     device = "none";
     fsType = "tmpfs";
-    options = [ "defaults" "size=64G" "mode=755" ];
+    options = [ "defaults" "size=32G" "mode=755" ];
   };
 
   fileSystems."/nix" = {
